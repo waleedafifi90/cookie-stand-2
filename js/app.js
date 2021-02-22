bd1acceb24a4e2cd43e5547656070af5c1e668f1
Israa-Ireqat <israa.ireqat333@gmail.com>
2021-02-22 10:48:31 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,28 +1,52 @@
+// 
+// 
 'use strict';
-let oclock= ['6 am', '7 am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm'];
-const Seattle = {   
-    minCustomers :23,
-    maxCustomers :65,
-    avgCookies :6.3,
-    cookies:[],
-     generateRandomNumber:function() {
-     this.cookies.push(getRandomNumber(this.minCustomers,this.maxCustomers)* this.avgCookies);
-       },
-       render:function() {
-    const parentElement = document.getElementById('Market');
-    const articleElement =document.createElement ('article');
-    parentElement.appendChild (articleElement);
-    const h2Element =document.createElement ('h2');
-    articleElement.appendChild (h2Element);
-    h2Element.textContent ='Seattle';
-    const ulElement =document.createElement('ul');
-    articleElement.appendChild(ulElement);
-     for (let i = 0; i < oclock.length; i++) {
-         const liElement =document.createElement('li');
-         ulElement.appendChild(liElement);
-         liElement.textContent =`${oclock[i]} : ${this.cookies[i]}`;
-     }
-     console.log(Seattle);
+// Seattle
+let workingHours= ['6 am', '7 am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm'];
+let total=0;
+const seattle = {
+  cityName: 'Seattle',
+  minHourlyCustomers: 23,
+  maxHourlyCustomers: 65,
+  avgCookiesSoldPerCustomer: 6.3,
+  customersPerHour: [],
+  cookiesSoldPerHour: [],
+//   totalCookiesSold: 0,
+  randomCustomer: function () {
+    this.customersPerHour = customers( this.minHourlyCustomers, this.maxHourlyCustomers );
+  },
+  render: function () {
+    const parentElement = document.getElementById( 'City' );
+    const h2Element = document.createElement( 'h2' );
+    parentElement.appendChild( h2Element );
+    h2Element.textContent = this.cityName;
+    const unOrderList = document.createElement( 'ul' );
+    parentElement.appendChild( unOrderList );
+    for( let i=0; i<14 ; i++ ){
+      const liElement=document.createElement( 'li' );
+      unOrderList.appendChild( liElement );
+      liElement.textContent= `${workingHours[i]}: ${Math.floor( this.customersPerHour[i]*this.avgCookiesSoldPerCustomer )}`;
+      total= total+ Math.floor( this.customersPerHour[i]*this.avgCookiesSoldPerCustomer );
     }
-    }
-    Seattle.render();
+    const liElement =document.createElement( 'li' );
+    unOrderList.appendChild( liElement );
+    liElement.textContent=`Total: ${total}`;
+    // for(let i = 0; i < this.likes.length; i++) {
+    //     const liElement = document.createElement('li');
+    //     ulElement.appendChild(liElement);
+    //     liElement.textContent = this.likes[i];
+    //   }
+  }
+};
+//Invoke
+seattle.randomCustomer();
+seattle.render();
+// Global Helper Function
+function customers( min, max ) {
+  let cuArr = [];
+  for ( let i = 0; i < 15; i++ ) {
+    let cu = Math.floor( Math.random() * ( max - min + 1 ) + min );
+    cuArr.push( cu );
+  }
+  return cuArr;
+}
